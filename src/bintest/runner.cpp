a174dcffa359b8329e47a8a9af23f9b7073a8a9d
Samael Wang <freesamael@gmail.com>
2010-03-31 10:05:58 +0000

    git-svn-id: file:///Volumes/Repositories/wolf-svn/trunk@20 bb1245a0-9139-df11-a8a8-0211d84427d1

---
+++ src/bintest/runner.cpp
@@ -0,0 +1,11 @@
+/*
+ * runner.cpp
+ *
+ *  Created on: Mar 27, 2010
+ *      Author: samael
+ */
+
+int main()
+{
+	return 0;
+}
---
+++ src/bintest/testudpclient.cpp
@@ -0,0 +1,45 @@
+/*
+ * udpclient.cpp
+ *
+ *  Created on: Mar 30, 2010
+ *      Author: samael
+ */
+
+#include <iostream>
+#include <string>
+#include "UDPSocket.h"
+#include "TLVReaderWriter.h"
+#include "TLVString.h"
+#include "HostAddress.h"
+
+using namespace std;
+using namespace cml;
+
+int main()
+{
+    string str;
+    UDPSocket sock;
+    TLVReaderWriter rw(&sock);
+    HostAddress addr;
+    unsigned short port;
+
+    sock.connect("127.0.0.1", 5566);
+    do {
+    	cin >> str;
+
+        // Write out.
+        TLVString ostr(str);
+        rw.write(ostr);
+
+        // Read in.
+        ITLVObject *obj;
+        TLVString *istr;
+        obj = rw.read();
+        if ((istr = dynamic_cast<TLVString *>(obj))) {
+            cout << istr->toString() << endl;
+        }
+        delete obj;
+    } while (str != "quit");
+
+    return 0;
+}
