9a63ee9f0412141ff09e05858f43b0bdd9a8b67e
Samael Wang <freesamael@gmail.com>
2010-04-21 14:03:00 +0000

    git-svn-id: file:///Volumes/Repositories/wolf-svn/trunk@104 bb1245a0-9139-df11-a8a8-0211d84427d1

---
+++ src/comlibinc/IRunnable.h
@@ -0,0 +1,26 @@
+/*
+ * IRunnable.h
+ *
+ *  Created on: Apr 21, 2010
+ *      Author: samael
+ */
+
+#ifndef IRUNNABLE_H_
+#define IRUNNABLE_H_
+
+namespace cml
+{
+
+/**
+ * \interface IRunnable
+ * IRunnable defines an interface of objects that can be executed by Thread.
+ */
+class IRunnable
+{
+public:
+	virtual void run() = 0;
+};
+
+}
+
+#endif /* IRUNNABLE_H_ */
