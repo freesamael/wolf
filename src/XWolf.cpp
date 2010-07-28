14158b670c342f61756c4b80575bff283ffac7b3
Samael Wang <freesamael@gmail.com>
2010-07-28 06:02:21 +0000

    git-svn-id: file:///Volumes/Repositories/wolf-svn/trunk@310 bb1245a0-9139-df11-a8a8-0211d84427d1

--- src/XWolf.cpp
+++ src/XWolf.cpp
@@ -4,10 +4,14 @@
  * \author samael
  */
 
+#include <cstdlib>
 #include "XWolf.h"
 
-#if defined(__GLIBC__) && defined(__GLIBCXX__)
+#if defined(__GLIBC__)
 #include <execinfo.h>
+#endif
+
+#if defined(__GLIBCXX__)
 #include <cxxabi.h>
 #endif
 
@@ -16,10 +20,47 @@ using namespace std;
 namespace wolf
 {
 
+/**
+ * The symbol string is like:
+ *     bin/manset_runner(_ZN4wolf5XWolfC2ERKSs+0x4b) [0x8085489]
+ *
+ * Hence, characters between '(' and '+' is the mangle string.
+ */
+string demangle(char *symbol)
+{
+#if defined(__GLIBCXX__)
+	int begin = 0, end = 0;
+
+	// Find begin.
+	for (int i = 0; symbol[i] != '\0'; i++)
+		if (symbol[i] == '(')
+			begin = i + 1;
+
+	// Find end.
+	for (int i = begin; symbol[i] != '\0'; i++)
+		if (symbol[i] == '+')
+			end = i;
+
+	// Demangle.
+	if (begin && end) {
+		string str;
+		symbol[end] = '\0';
+		int status;
+		char *cstr = abi::__cxa_demangle(&symbol[begin], NULL, NULL, &status);
+		str = cstr;
+		free(cstr);
+		symbol[end] = '+';
+		return str;
+	}
+#endif
+
+	return symbol;
+}
+
 XWolf::XWolf(const string &remark) throw():
 		_estr(remark)
 {
-#if defined(__GLIBC__) && defined(__GLIBCXX__)
+#if defined(__GLIBC__)
 	int nptrs;
 	void *buf[128];
 	char **cstrs;
@@ -29,10 +70,12 @@ XWolf::XWolf(const string &remark) throw():
 
 	_estr += "\n\tat ";
 	for (int i = 0; i < nptrs; i++) {
-		_estr += cstrs[i];
+		_estr += demangle(cstrs[i]);
 		if (i != nptrs - 1)
 			_estr += "\n\tby ";
 	}
+
+	free(cstrs);
 #endif
 }
 
